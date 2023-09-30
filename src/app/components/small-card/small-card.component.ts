35911477b3ddcd853153081a7a5619d5c72189e8
tiago <tiagofoa.ti@gmail.com>
2023-09-30 20:31:19 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -13,6 +13,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  Id:string = "0"
 
   ngOnInit(): void {
   }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Trilogia Senhor dos Anéis versão extendida",
+        "description": "ola",
+        "photoCover":"https://t.ctcdn.com.br/qla8r-ACpAxwjvvinEUvANlV0N0=/768x432/smart/i445272.jpeg"
+    },
+    {
+        "id":"2",
+        "title": "Gandalf pode ser Iru Ilúvatar, o deus da Terra Média",
+        "description": "ola",
+        "photoCover":"https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/05/Gandalf-the-White.jpg?q=50&fit=crop&w=750&dpr=1.5"
+    }
+]
