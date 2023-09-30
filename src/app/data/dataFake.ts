89898d121b21050af3aa87186d94c2b12584a449
tiago <tiagofoa.ti@gmail.com>
2023-09-30 20:47:30 -0300

    content updated

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,27 @@ export const dataFake = [
     {
         "id":"1",
         "title": "Trilogia Senhor dos Anéis versão extendida",
-        "description": "ola",
+        "description": "No formato ampliado, A Sociedade do Anel (2001) ganha 30 minutos a mais, ficando assim com quase 3h30. As Duas Torres (2002), que inicialmente durava 3h, passa a incluir mais 42 minutos. Já O Retorno do Rei (2003) se excede em 48 minutos, totalizando pouco mais de 4h.",
         "photoCover":"https://t.ctcdn.com.br/qla8r-ACpAxwjvvinEUvANlV0N0=/768x432/smart/i445272.jpeg"
     },
     {
         "id":"2",
         "title": "Gandalf pode ser Iru Ilúvatar, o deus da Terra Média",
-        "description": "ola",
+        "description": "Tolkien explica que Gandalf é originalmente um Maia, um espírito primordial criado para ajudar os Valar a moldar o mundo. No entanto, alguns sugeriram que Gandalf pode ser a personificação de Iru Ilúvatar, o deus todo-poderoso da Terra-média, que veio ao mundo (usando a forma física de Gandalf como veículo) para ajudar a destruir Sauron, que também costumava ser uma Maia.",
         "photoCover":"https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/05/Gandalf-the-White.jpg?q=50&fit=crop&w=750&dpr=1.5"
+    },
+
+    {
+        "id":"3",
+        "title": "Assistir a trilogia completa leva 12 horas e 1 minuto",
+        "description": "Temos um total de 1032 minutos, ou seja, pouco mais de 17 horas. Dito isso, você precisa de menos de um dia para estar em dia com os filmes da saga.",
+        "photoCover":"https://segredosdomundo.r7.com/wp-content/uploads/2021/06/curiosidades-sobre-o-senhor-dos-aneis-famosa-trilogia-completa-20-anos-4.jpg.webp"
+    },
+    
+    {
+        "id":"4",
+        "title": "Legolas nunca fala com Frodo",
+        "description": "Após o elfo oferecer seu arco para a Sociedade do Anel (o que quase não pode ser considerado uma interação por si só), os dois nunca conversam novamente.",
+        "photoCover":"https://segredosdomundo.r7.com/wp-content/uploads/2021/06/curiosidades-sobre-o-senhor-dos-aneis-famosa-trilogia-completa-20-anos-2.jpg.webp"
     }
 ]
